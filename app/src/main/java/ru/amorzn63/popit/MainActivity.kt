9ff44513b60e7835b11ea686a5c5612617ea820e
AndreyOvch <amo-rzn@ya.ru>
2021-09-22 00:15:19 +0300

    Initial commit

---
+++ app/src/main/java/ru/amorzn63/popit/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.amorzn63.popit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.amorzn63.popit.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/amorzn63/popit/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.amorzn63.popit.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
