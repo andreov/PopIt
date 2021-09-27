9a0bc699ca35027769241723a541d0f6f2d2b0b0
AndreyOvch <amo-rzn@ya.ru>
2021-09-27 23:59:30 +0300

    Create ViewModel

--- app/src/main/java/ru/amorzn63/popit/ui/main/MainFragment.kt
+++ app/src/main/java/ru/amorzn63/popit/ui/main/MainFragment.kt
@@ -2,11 +2,17 @@ package ru.amorzn63.popit.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.HapticFeedbackConstants
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.fragment.app.viewModels
+import androidx.gridlayout.widget.GridLayout
 import ru.amorzn63.popit.R
+import ru.amorzn63.popit.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
@@ -14,19 +20,45 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: MainFragmentBinding // отложенная инициализация binding
+    private val viewModel: MainViewModel by viewModels()  // делегат для работы с ViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.state.observe(viewLifecycleOwner) {
+            binding.field.removeAllViews()
+            it.forEachIndexed { index, state ->
+                val row = index / 5
+                val colum = index % 5
+                val params = GridLayout.LayoutParams(  //задаются координаты пузыря для View
+                    GridLayout.spec(row, 1f),
+                    GridLayout.spec(colum, 1f)
+                )
+                // отрисовка элемента
+                val item = ImageView(requireContext()).apply {
+                    setImageResource(R.drawable.ic_selected)
+                    setBackgroundResource(R.drawable.backgraud_item)
+                    isEnabled = state                   // состояние вкл выкл
+                    isHapticFeedbackEnabled = true   // вкл. вибрации
+                    setOnClickListener { v ->
+                        v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+                        viewModel.onPopClick(index)
+
+                    }
+                }
+                binding.field.addView(item, params)
+            }
+        }
     }
+
 
 }
--- app/src/main/java/ru/amorzn63/popit/ui/main/MainViewModel.kt
+++ app/src/main/java/ru/amorzn63/popit/ui/main/MainViewModel.kt
@@ -1,7 +1,25 @@
 package ru.amorzn63.popit.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private lateinit var matrix:Array<Boolean> // матрица состояний пузырей
+
+    private val mState = MutableLiveData<Array<Boolean>>() // изменяемая для работы внутри класса
+    val state:LiveData<Array<Boolean>> = mState // неизменяемая для передачи во View
+
+    init {
+        initGame()
+    }
+
+    private  fun initGame(){
+        matrix = Array(25) {true} // инициализация матрицы
+        mState.value = matrix
+    }
+
+    fun onPopClick(index: Int) {
+
+    }
 }
